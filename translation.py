7fb43980388cfca36a1e02dde79d77498f3836c0
Kubinic <106214811+Kubinic@users.noreply.github.com>
2022-06-04 19:32:30 +0700

    Delete translation.py

--- translation.py
+++
@@ -1,8 +0,0 @@
-rus_eng = {
-    'one': 'один', 'two': 'два', 'three': 'три', 'four': 'четыре', 'five': 'пять', 'six': 'шесть',
-    'seven': 'семь', 'eight': 'восемь', 'nine': 'девять', 'ten': 'десять'
-}
-
-def num_translate(eng_word):
-    return rus_eng.get(eng_word)
-print(num_translate('two'))
